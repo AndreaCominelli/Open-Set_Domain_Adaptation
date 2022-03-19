410554d2578278394c180f5523ee93f16a477c14
AndreaCominelli <andreacominelli56@gmail.com>
2022-03-19 12:46:45 +0100

    test

--- training_utilities.py
+++ training_utilities.py
@@ -2,18 +2,6 @@
 # https://en.wikipedia.org/wiki/Reciprocal_distribution
 
 import math
-import numpy as npy
+from scipy.stats import expon
 
-def log_uniform(a, b):
-    pdfs = []
-    range = npy.linspace(a, b)
-    for x in list(range):
-        f = 1 / (x * (math.log(b) - math.log(a)))
-        pdfs.append(f)
-    return pdfs
-
-if __name__ == "__main__":
-    a = 1 * 10 ** -4
-    b = 1 * 10 ** -1
-    res = log_uniform(a, b)
-    print(res)
+print(expon(scale = 100))
