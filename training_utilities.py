d74305b3706ea3fdd5620495bd517e459f0f4125
AndreaCominelli <andreacominelli56@gmail.com>
2022-03-19 12:52:55 +0100

    test

--- training_utilities.py
+++ training_utilities.py
@@ -2,6 +2,6 @@
 # https://en.wikipedia.org/wiki/Reciprocal_distribution
 import numpy as npy
 
-x = npy.random.exponential(scale=0.5, size=5)
+x = npy.random.exponential(scale=0.05, size=5)
 
 print(x)
