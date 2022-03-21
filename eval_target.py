ffd0946694e398484e5b31050445f706f9002ae2
AndreaCominelli <andreacominelli56@gmail.com>
2022-03-21 12:42:28 +0100

    added params

--- eval_target.py
+++ eval_target.py
@@ -62,10 +62,10 @@ def evaluation(feature_extractor, self_cls, multi_head, n_classes_known, thresho
             normality_scores.append(normality_score)
 
             if normality_score > threshold:
-                print(f"IMAGE PATH ----> {img_path[0] + ' ' + str(class_l[0])}")
+                print(f"IMAGE PATH ----> {img_path[0] + ' ' + str(class_l.item())}")
                 known_samples.append(img_path[0] + ' ' + str(class_l))
             else:
-                print(f"IMAGE PATH ----> {img_path[0] + ' ' + str(class_l[0])}")
+                print(f"IMAGE PATH ----> {img_path[0] + ' ' + str(class_l.item())}")
                 unknown_samples.append(img_path[0] + ' ' + str(class_l))
     
     auroc = roc_auc_score(ground_truth, normality_scores)
